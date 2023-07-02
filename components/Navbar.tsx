bf771de6d8342ecac8015fdb8e9d3f68d91b2ebd
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-07-02 17:46:32 +0800

    initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="grid w-full h-full grid-cols-3">
+      <div></div>
+      <div>
+        <h1>Urban Garden</h1>
+      </div>
+      <div>₱0.00</div>
+    </nav>
+  );
+};
+
+export default Navbar;
