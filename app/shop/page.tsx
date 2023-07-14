7e3e0a45d68d24cba880f89843d819f89ec29c9a
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-07-14 10:36:20 +0800

    added plantlist component to shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,15 +1,27 @@
+import { PlantList } from "@/components";
+import { fetchPlants } from "@/utils";
 import React from "react";
 
-const page = () => {
+export default async function page() {
+  const allPlantsObject = await fetchPlants();
+
+  const isDataEmpty = !allPlantsObject || allPlantsObject.length < 1;
+
+  const filteredPlants = allPlantsObject?.slice(0, 9) ?? [];
+
   return (
-    <section className="bg-gray-400 w-full h-full flex justify-center items-center py-6 px-6">
-      <div className="bg-white">
-        <span className="flex justify-start items-start text-gray-300">
+    <section className="bg-gray-100 w-full h-full flex justify-center items-center py-6 px-4 max-width">
+      <div className="bg-white w-full h-full px-6 py-4">
+        <span className="flex justify-start items-start text-gray-500 text-sm">
           Home / Shop
         </span>
+        <div className="flex justify-between items-center">
+          <p>Showing 1 - 9 of 25 results</p>
+        </div>
+        <div className="flex mt-10">
+          <PlantList filteredPlants={filteredPlants} />
+        </div>
       </div>
     </section>
   );
-};
-
-export default page;
+}
--- components/PlantList.tsx
+++ components/PlantList.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import React from "react";
 import PlantCard from "./PlantCard";
 import { PlantProps } from "@/types";
+import { usePathname } from "next/navigation";
 
 interface PlantListProps {
   filteredPlants: PlantProps[];
 }
 
 const PlantList = ({ filteredPlants }: PlantListProps) => {
+  const pathname = usePathname();
   return (
     <section className="flex flex-col items-start justify-start w-full px-6 pt-10 pb-10 sm:pb-20 lg:px-20 max-width">
-      <div className="flex items-center w-full sm:justify-start xs:justify-center ">
+      <div
+        className={`${
+          pathname === "/shop" ? "hidden" : "flex"
+        } items-center w-full sm:justify-start xs:justify-center`}
+      >
         <h1 className="xs:text-[34px] sm:text-[40px]">New Plants</h1>
       </div>
       <div className="flex flex-wrap items-center mt-10 justify-evenly gap-y-10 gap-x-2 sm:gap-x-4 md:gap-x-2">
